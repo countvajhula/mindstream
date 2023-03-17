6aec37d70e7834c7d7e961760a60d47040b8f270
Siddhartha <sid@countvajhula.com>
2023-03-17 10:59:25 -0700

    remove duplicated defcustom

--- mindstream-custom.el
+++ mindstream-custom.el
@@ -32,11 +32,6 @@
   "A scratch buffer."
   :group 'Editing)
 
-(defcustom mindstream-path "/var/tmp/racket/"
-  "Directory path where mindstream buffers will be saved during development."
-  :type 'string
-  :group 'mindstream)
-
 (defcustom mindstream-path "/var/tmp/racket/" ; TODO: make platform-independent?
   "Directory path where mindstream buffers will be saved during development."
   :type 'string
