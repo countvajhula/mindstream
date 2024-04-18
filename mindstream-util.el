768423046332110cb14fc6293220eec8bf4fb23c
Siddhartha <sid@countvajhula.com>
2024-04-17 17:26:52 -0700

    update header: ABE, etc.

--- mindstream-util.el
+++ mindstream-util.el
@@ -5,19 +5,28 @@
 ;; Version: 0.0
 ;; Keywords: convenience, files, languages, outlines, tools, vc, wp
 
-;; This program is "part of the world," in the sense described at
-;; https://drym.org.  From your perspective, this is no different than
-;; MIT or BSD or other such "liberal" licenses that you may be
-;; familiar with, that is to say, you are free to do whatever you like
-;; with this program.  It is much more than BSD or MIT, however, in
-;; that it isn't a license at all but an idea about the world and how
-;; economic systems could be set up so that everyone wins.  Learn more
-;; at drym.org.
-;;
-;; This work transcends traditional legal and economic systems, but
-;; for the purposes of any such systems within which you may need to
-;; operate:
-;;
+;; This work is "part of the world." You are free to do whatever you
+;; like with it and it isn't owned by anybody, not even the creators.
+;; Attribution would be appreciated and would help, but it is not
+;; strictly necessary nor required.
+
+;; The freely released, copyright-free work in this repository
+;; represents an investment in a better way of doing things called
+;; attribution-based economics (ABE). Attribution-based economics is
+;; based on the simple idea that we gain more by giving more, not by
+;; holding on to things that, truly, we could only create because we,
+;; in our turn, received from others. As it turns out, an economic
+;; system based on attribution -- where those who give more are more
+;; empowered -- is significantly more efficient than capitalism while
+;; also being stable and fair (unlike capitalism, on both counts),
+;; giving it transformative power to elevate the human condition and
+;; address the problems that face us today along with a host of others
+;; that have been intractable since the beginning. You can help make
+;; this a reality by releasing your work in the same way -- freely
+;; into the public domain in the simple hope of providing value. Learn
+;; more about attribution-based economics at drym.org, tell your
+;; friends, do your part.
+
 ;; This is free and unencumbered software released into the public domain.
 ;; The authors relinquish any copyright claims on this work.
 ;;
